010c560977555aa5643d84beb71e3d9ec998e15e
toshi0383 <t.suzuki326@gmail.com>
2022-10-22 10:26:38 +0900

    chore: Documentations

--- Example/ViewController.swift
+++ Example/ViewController.swift
@@ -3,35 +3,13 @@ import UIKit
 
 final class ViewController: UIViewController {
 
-    let child = ChildViewController()
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        addChild(child)
-        view.addSubview(child.view)
-        child.view.translatesAutoresizingMaskIntoConstraints = false
-
-        NSLayoutConstraint.activate([
-            child.view.topAnchor.constraint(equalTo: view.topAnchor),
-            child.view.bottomAnchor.constraint(equalTo: view.bottomAnchor),
-            child.view.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-            child.view.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-        ])
-
-        child.didMove(toParent: self)
-    }
-}
-
-/// Making sure that it works inside childVC.
-final class ChildViewController: UIViewController {
-
     private let manager = InstructionManager(overlayBackgroundColor: .overlayBackground)
+
+    private var isShowingInstruction = false
 
     private let label1 = UIButton()
     private let label2 = UIButton()
     private let label3 = UIButton()
-    private var isShowingInstruction = false
 
     private let vStack: UIStackView = {
         let vStack = UIStackView()
@@ -42,60 +20,12 @@ final class ChildViewController: UIViewController {
         return vStack
     }()
 
+    // MARK: LifeCycle
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
         configureLayout()
-    }
-
-    // MARK: Layout
-
-    private func configureLayout() {
-
-        view.backgroundColor = .background
-
-        do {
-            label1.setTitleColor(.systemRed, for: .normal)
-            label1.setTitle("Hello", for: .normal)
-            label1.backgroundColor = .systemGreen
-            label1.addTarget(self, action: #selector(tap1), for: .touchUpInside)
-        }
-
-        do {
-            label2.backgroundColor = .systemCyan
-            label2.setTitleColor(.systemBlue, for: .normal)
-            label2.setTitle("Tap", for: .normal)
-            label2.addTarget(self, action: #selector(tap2), for: .touchUpInside)
-        }
-
-        do {
-            label3.backgroundColor = .systemYellow
-            label3.setTitleColor(.systemBlue, for: .normal)
-            label3.setTitle("Custom Next", for: .normal)
-            label3.addTarget(self, action: #selector(tap3), for: .touchUpInside)
-        }
-
-        [label1, label2, label3].forEach {
-            $0.widthAnchor.constraint(equalToConstant: 100).isActive = true
-            $0.heightAnchor.constraint(equalToConstant: 100).isActive = true
-            $0.translatesAutoresizingMaskIntoConstraints = false
-            vStack.addArrangedSubview($0)
-        }
-
-        vStack.frame = CGRect(origin: originalVStackPoint, size: CGSize(width: 100, height: 300))
-        vStack.addGestureRecognizer(UIPanGestureRecognizer(target: self, action: #selector(handlePan)))
-        view.addSubview(vStack)
-    }
-
-    private var originalVStackPoint: CGPoint = .init(x: 50, y: 50)
-
-    @objc private func handlePan(_ panGesture: UIPanGestureRecognizer) {
-        if panGesture.state == .began {
-            originalVStackPoint = vStack.frame.origin
-        }
-        let t = panGesture.translation(in: view)
-        let o = originalVStackPoint
-        vStack.frame.origin = CGPoint(x: t.x + o.x, y: t.y + o.y)
     }
 
     // MARK: Invoking Yokoso Instructions
@@ -110,54 +40,24 @@ final class ChildViewController: UIViewController {
         isShowingInstruction = true
 
         do {
+
             try manager.show(
                 instruction,
                 in: view
             ) { [weak self] success in
                 guard let me = self else { return }
 
+                // called when instruction "did" finish
+
                 me.isShowingInstruction = false
-                onFinish?(success)
             }
+
         } catch {
             isShowingInstruction = false
+
             if let error = error as? InstructionError {
                 showError(error)
             }
-        }
-    }
-
-    private func showError(_ error: InstructionError) {
-
-        let alert = UIAlertController(title: "Error", message: """
-        \(error)
-        You get this error when sourceView is not fully visible inside window bounds.
-        Remember that same situation applys on window size changes(device rotation or iPad SplitView variants).
-        NOTE: This error message is demonstration purpose.
-        """, preferredStyle: .alert)
-
-        alert.addAction(.init(title: "OK", style: .default))
-        present(alert, animated: true)
-    }
-
-    @objc private func tap1() {
-        print(#function)
-        if !isShowingInstruction {
-            startI1()
-        }
-    }
-
-    @objc private func tap2() {
-        print(#function)
-        if !isShowingInstruction {
-            startI2()
-        }
-    }
-
-    @objc private func tap3() {
-        print(#function)
-        if !isShowingInstruction {
-            startI3()
         }
     }
 
@@ -247,5 +147,93 @@ final class ChildViewController: UIViewController {
 
     @objc private func skipByCustomButton() {
         manager.close()
+    }
+
+    // MARK: Layout
+
+    private func configureLayout() {
+
+        view.backgroundColor = .background
+
+        do {
+            label1.setTitleColor(.systemRed, for: .normal)
+            label1.setTitle("Hello", for: .normal)
+            label1.backgroundColor = .systemGreen
+            label1.addTarget(self, action: #selector(tap1), for: .touchUpInside)
+        }
+
+        do {
+            label2.backgroundColor = .systemCyan
+            label2.setTitleColor(.systemBlue, for: .normal)
+            label2.setTitle("Tap", for: .normal)
+            label2.addTarget(self, action: #selector(tap2), for: .touchUpInside)
+        }
+
+        do {
+            label3.backgroundColor = .systemYellow
+            label3.setTitleColor(.systemBlue, for: .normal)
+            label3.setTitle("Custom Next", for: .normal)
+            label3.addTarget(self, action: #selector(tap3), for: .touchUpInside)
+        }
+
+        [label1, label2, label3].forEach {
+            $0.widthAnchor.constraint(equalToConstant: 100).isActive = true
+            $0.heightAnchor.constraint(equalToConstant: 100).isActive = true
+            $0.translatesAutoresizingMaskIntoConstraints = false
+            vStack.addArrangedSubview($0)
+        }
+
+        vStack.frame = CGRect(origin: originalVStackPoint, size: CGSize(width: 100, height: 300))
+        vStack.addGestureRecognizer(UIPanGestureRecognizer(target: self, action: #selector(handlePan)))
+        view.addSubview(vStack)
+    }
+
+    // MARK: Handing Gesture
+
+    private var originalVStackPoint: CGPoint = .init(x: 50, y: 50)
+
+    @objc private func handlePan(_ panGesture: UIPanGestureRecognizer) {
+        if panGesture.state == .began {
+            originalVStackPoint = vStack.frame.origin
+        }
+        let t = panGesture.translation(in: view)
+        let o = originalVStackPoint
+        vStack.frame.origin = CGPoint(x: t.x + o.x, y: t.y + o.y)
+    }
+
+    @objc private func tap1() {
+        print(#function)
+        if !isShowingInstruction {
+            startI1()
+        }
+    }
+
+    @objc private func tap2() {
+        print(#function)
+        if !isShowingInstruction {
+            startI2()
+        }
+    }
+
+    @objc private func tap3() {
+        print(#function)
+        if !isShowingInstruction {
+            startI3()
+        }
+    }
+
+    // MARK: Utilities
+
+    private func showError(_ error: InstructionError) {
+
+        let alert = UIAlertController(title: "Error", message: """
+        \(error)
+        You get this error when sourceView is not fully visible inside window bounds.
+        Remember that same situation applys on window size changes(device rotation or iPad SplitView variants).
+        NOTE: This error message is demonstration purpose.
+        """, preferredStyle: .alert)
+
+        alert.addAction(.init(title: "OK", style: .default))
+        present(alert, animated: true)
     }
 }
--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Yokoso
 
-Simple spotlight instruction framework for UIKit iOS.
+Yet another super simple spotlight instruction framework for UIKit iOS.
 
 ## Why Yokoso?
 
@@ -13,12 +13,107 @@ Simple spotlight instruction framework for UIKit iOS.
 
 https://user-images.githubusercontent.com/6007952/197309254-e7a0ed85-89c0-4d5a-a022-6e02c1fadb3b.mp4
 
-## Sample
+## How to use
 
-Please refer to example app inside repository.
+It's super simple.
+Import the module,
+
+```swift
+import Yokoso
+```
+
+initialize `InstructionManager`,
+
+```swift
+    let manager = InstructionManager(overlayBackgroundColor: .overlayBackground)
+```
+
+then use `InstructionManager`'s interface to show or close your `Instruction`.
+
+### Showing single Instruction
+
+To simplify internal implementation, we don't check view state internally, but just redrawing everything again.
+So make sure you know `isShowingInstruction` state to avoid calling `show()` multiple time.
+It's safe to invoke `show` while showing, but it would trigger overlay's fade animation again.
+
+```swift
+        if isShowingInstruction { return }
+
+        isShowingInstruction = true
+
+        do {
+
+            try instructionManager.show(
+                .init(
+                    message: .init(
+                        attributedString: attributedString,
+                        backgroundColor: uicolor
+                    ),
+                    nextButton: .simple("Next"),
+                    sourceView: label
+                ),
+                in: view
+            ) { [weak self] success in
+                guard let me = self else { return }
+
+                // called when instruction "did" finish
+
+                me.isShowingInstruction = false
+            }
+
+        } catch {
+
+            isShowingInstruction = false
+
+            if let error = error as? InstructionError {
+                showError(error)
+            }
+        }
+```
+
+### Showing multiple Instructions
+
+Yokoso is stateless.
+If you want to show multiple instructions in order, then call `show()` in order.
+This way you have more control over the timings, comparing to passing multiple `Instruction` at once.
+
+```swift
+    private func startI1() {
+        show(
+            .init(
+                message: .init(attributedString: makeMessage("Hi with simple Next button. Tap anywhere to continue."), backgroundColor: .background),
+                nextButton: .simple("Next"),
+                sourceView: label1
+            )
+        ) { [weak self] success in
+
+            self?.startI2() // NOTE: Starting next instruction.
+
+        }
+    }
+
+    private func startI2() {
+        show(
+            .init(
+            ...
+```
+
+Please refer to example app inside repository for further customization.
 
 ## Install
 
 - [x] Swift Package Manager
 - [ ] CocoaPods
 - [ ] Carthage
+
+## Contributing
+
+Any contributions are warmly welcomed.😊
+
+- Feature Request / Pull Request
+- Bug Report
+- Question!
+
+## LICENSE
+
+MIT
